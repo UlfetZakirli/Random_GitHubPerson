c8d669289b4feac34812da355e4965fd42c80fed
UlfetZakirli <ulfetzakirli@gmail.com>
2022-04-15 12:01:46 +0400

    myReact

---
+++ setup/src/data.js
@@ -0,0 +1,60 @@
+const reviews = [
+  {
+    id: 1,
+    name: 'ElxanQuliyev',
+    job: 'CEO',
+    image:
+      'https://avatars.githubusercontent.com/u/47784245?v=4',
+    text:
+      'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+  },
+  {
+    id: 2,
+    name: 'HikmetXidirzada57',
+    job: 'WEB DEVELOPER',
+    image:
+      'https://avatars.githubusercontent.com/u/101005035?v=4',
+    text:
+      'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+  },
+  {
+    id: 3,
+    name: 'Abdurahmanov737',
+    job: 'WEB DEVELOPER',
+    image:
+      'https://avatars.githubusercontent.com/u/85895078?v=4',
+    text:
+      'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+  },
+  {
+    id: 4,
+    name: 'zeynebss',
+    job: 'WEB DEVELOPER',
+    image:
+      'https://avatars.githubusercontent.com/u/88382394?v=4',
+    text:
+      'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+  },
+  {
+    id: 5,
+    name: 'UlfetZakirli',
+    job: 'web developer',
+    image:
+      'https://avatars.githubusercontent.com/u/88549805?s=400&u=85cb1880202c9745e80734c651810f546a92d417&v=4',
+    text:
+      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+ 
+  {
+    id: 6,
+    name: 'Aqsin Heyderli',
+    job: 'WEB DEVELOPER',
+    image:
+      'https://avatars.githubusercontent.com/u/84628205?v=4',
+    text:
+      'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+  },
+  
+];
+
+export default reviews;
